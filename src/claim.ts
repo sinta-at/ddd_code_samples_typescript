2d5af73b2cc8468b08575e9db8868e0b944b8795
Paul Rayner <paul@virtualgenius.com>
2021-08-31 19:40:00 -0600

    Update comments for Claim

--- src/claim.ts
+++ src/claim.ts
@@ -2,7 +2,11 @@ import {v4 as uuidv4} from 'uuid';
 
 import RepairPO from './repair_po';
 
-// Claim represents a request for a benefit on an extended warranty
+/**
+ Claim represents a request for a benefit on an extended warranty. It contains a
+ set of purchase orders that provide information about any repairs and associated costs that may have occurred for a claim.
+*/
+
 export default class Claim {
   id:                    string; // unique id
   readonly amount:       number;
