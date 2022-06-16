c0f36b6e49802117534bbc0e3f2861d77513e204
Sinta AT <sinta.tangkudung@xendit.co>
2022-06-16 10:55:42 +0700

    Add rules for purchase date, effective date, and expiration date

--- src/terms_and_conditions.ts
+++ src/terms_and_conditions.ts
@@ -6,6 +6,13 @@ export default class TermsAndConditions {
 	constructor(purchase_date: Date,
 		effective_date: Date,
 		expiration_date: Date) {
+		if (purchase_date > effective_date) {
+			throw new Error("Effective date should be after or the same as purchase date")
+		}
+		if (effective_date >= expiration_date) {
+			throw new Error("Expiration date should be after effective date")
+		}
+
 		this.purchase_date = purchase_date;
 		this.effective_date = effective_date;
 		this.expiration_date = expiration_date;
